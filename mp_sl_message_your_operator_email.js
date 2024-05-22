20c19ceb95f938a713f56ad13aa93fc11d2cce41
ankith88 <ankith88@gmail.com>
2024-05-22 15:21:20 +1000

    CU-86cvf02uz

---
+++ mp_sl_message_your_operator_email.js
@@ -0,0 +1,97 @@
+/*
+ * Author:               Ankith Ravindran
+ * Created on:           Wed May 22 2024
+ * Modified on:          Wed May 22 2024 15:15:17
+ * SuiteScript Version:  1.0 
+ * Description:          Send email to the franchisee including the message from the customer when "Message Your Operator" button is clicked from the customer portal.  
+ *
+ * Copyright (c) 2024 MailPlus Pty. Ltd.
+ */
+
+
+var ctx = nlapiGetContext();
+
+var zee = 0;
+var role = ctx.getRole();
+var row_count = 0;
+var customer_list_page = null;
+if (role == 1000) { //Role is Franchisee
+    zee = ctx.getUser(); //Get Franchisee ID
+} else {
+    zee = 0;
+}
+
+function emailYourOperator(request, response) {
+    if (request.getMethod() == "GET") {
+
+        nlapiLogExecution('DEBUG', 'request.getParameter', request.getParameter);
+
+        var email = request.getParameter('email');
+        var customerInternalId = request.getParameter('customerInternalId');
+        var message = request.getParameter('message');
+
+        nlapiLogExecution('DEBUG', 'email', email);
+        nlapiLogExecution('DEBUG', 'customerInternalId', customerInternalId);
+        nlapiLogExecution('DEBUG', 'message', message);
+
+        var params = {
+            email: email,
+            customerInternalId: customerInternalId,
+            message: message
+        };
+
+
+        var customerRecord = nlapiLoadRecord('customer', customerInternalId);
+        var entity_id = customerRecord.getFieldValue('entityid');
+        var entitystatus = customerRecord.getFieldValue('entitystatus');
+        var business_name = customerRecord.getFieldText('companyname');
+        var partner_text = customerRecord.getFieldText('partner');
+        var partner_id = customerRecord.getFieldValue('partner');
+
+        var partner_email = nlapiLoadRecord('partner', partner_id).getFieldValue('email');
+        var partner_phone = nlapiLoadRecord('partner', partner_id).getFieldValue('custentity2');
+
+        var emailAttach = new Object();
+        emailAttach['entity'] = customerInternalId;
+
+        // Email to be sent out to Corrine about the new LPO Lead.
+        var from = 112209; //MailPlus team
+        var to = partner_email;
+        var bcc = ['dispatcher@mailplus.com.au'];
+        var emailSubject = 'Message from your Customer - ' + business_name;
+
+        var emailBody =
+            'Message : ' + message;
+
+
+        nlapiSendEmail(from, to, emailSubject, emailBody, null, bcc, emailAttach, null, true);
+
+
+        var returnObj = {
+            success: true,
+            message: '',
+            result: ''
+        };
+
+        _sendJSResponse(request, response, returnObj);
+
+    }
+
+    function _sendJSResponse(request, response, respObject) {
+        response.setContentType('JAVASCRIPT');
+        // response.setHeader('Access-Control-Allow-Origin', '*');
+        var callbackFcn = request.getParameter("jsoncallback") || request.getParameter('callback');
+        if (callbackFcn) {
+            response.writeLine(callbackFcn + "(" + JSON.stringify(respObject) + ");");
+        } else response.writeLine(JSON.stringify(respObject));
+    }
+}
+
+function getDate() {
+    var date = new Date();
+    if (date.getHours() > 6) {
+        date = nlapiAddDays(date, 1);
+    }
+    date = nlapiDateToString(date);
+    return date;
+}
